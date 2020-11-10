13e6761a1a5239ee4d20cfafedd339b7f34392af
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-11-10 15:43:39 +0530

    Fixed UI

--- codeplay/player/views.py
+++ codeplay/player/views.py
@@ -5,6 +5,8 @@ import os
 from youtube_search import YoutubeSearch
 from pytube import YouTube
 from .models import Song
+import json
+
 # Create your views here.
 mpath=settings.MEDIA_ROOT
 def index(request):
@@ -21,12 +23,20 @@ def index(request):
         data['tumbnails']=results[0]['thumbnails']
         data['duration']=results[0]['duration']
         data['views']=results[0]['views']
+        with open('data.txt', 'w') as json_file:
+            json.dump(data, json_file)
         return JsonResponse(data,safe=False)
+    with open('data.txt','r') as f:
+        data = json.load(f)
+    track=data['track']
+    thumbnails=data['tumbnails']
+    views=data['views']
+    link=data['text']
     path = settings.MEDIA_ROOT
     song_list = os.listdir(path + '/')
     if song_list:
         song_list=song_list[0]
     else:
         song_list=""
-    context={'url': song_list}
+    context={'url': song_list,'track': track,'tumbnails':thumbnails,'views':views,'link':link}
     return render(request,'index.html',context)
