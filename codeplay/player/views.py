1509b7833371bc758525dcb62698f727142330fc
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-11-10 12:11:27 +0530

    Back-End-Fixed

--- codeplay/player/views.py
+++ codeplay/player/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
 import os
-from bs4 import BeautifulSoup
+from youtube_search import YoutubeSearch
 from pytube import YouTube
-
+from .models import Song
 # Create your views here.
+mpath=settings.MEDIA_ROOT
 def index(request):
-    url="https://www.youtube.com/results?search_query="
+    url="https://www.youtube.com/"
     data={}
     if request.method=='POST':
         text=request.POST.get('text')
-        print(text)
-        data['text']=url+text
+        results = YoutubeSearch(text, max_results=10).to_dict()
+        yt=YouTube(url+results[0]['url_suffix'])
+        stream=yt.streams.filter(only_audio=True).first()
+        file=stream.download(mpath,filename='temp')
+        data['text']=url+results[0]['url_suffix']
         return JsonResponse(data,safe=False)
     path = settings.MEDIA_ROOT
     song_list = os.listdir(path + '/')
-    song_list=song_list[0]
+    if song_list:
+        song_list=song_list[0]
+    else:
+        song_list=""
     context={'url': song_list}
     return render(request,'index.html',context)
