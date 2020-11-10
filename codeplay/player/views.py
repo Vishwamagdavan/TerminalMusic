d5af38b515a42301859e6ac6798fc42ef09a3e45
Vishwamagdavan <vishwamagadavan@gmail.com>
2020-11-10 09:20:42 +0530

    Added Ajax to Django

--- codeplay/player/views.py
+++ codeplay/player/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.conf import settings
 import os
+from bs4 import BeautifulSoup
+from pytube import YouTube
+
 # Create your views here.
 def index(request):
+    url="https://www.youtube.com/results?search_query="
+    data={}
+    if request.method=='POST':
+        text=request.POST.get('text')
+        print(text)
+        data['text']=url+text
+        return JsonResponse(data,safe=False)
     path = settings.MEDIA_ROOT
     song_list = os.listdir(path + '/')
     song_list=song_list[0]
-    print(song_list)
     context={'url': song_list}
     return render(request,'index.html',context)
